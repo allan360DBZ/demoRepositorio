46991a36439d214de30995fd00dc523a8b0ed448
Allan <Allan@prive-a85e5968b>
2018-10-15 13:02:07 -0600

    primer cambio

---
+++ src/prueba/github/PruebaGithub.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba.github;
+
+/**
+ *
+ * @author Allan
+ */
+public class PruebaGithub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Principal p = new Principal();
+        p.setLocationRelativeTo(null);
+        p.setVisible(true);
+    }
+    
+}
